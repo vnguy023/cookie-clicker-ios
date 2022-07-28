eb9c22ab82da9a5074ab756c37ce0465168259e3
Vincent Nguyen <vincentnguyen88@hotmail.com>
2022-07-28 15:57:12 -0700

    - splashScene now has a logo animation and after completion will auto change to menu scene

--- cookie-clicker-ios/ChildViews/SplashView.swift
+++ cookie-clicker-ios/ChildViews/SplashView.swift
@@ -3,36 +3,46 @@ import SwiftUI
 struct SplashView: View {
     @EnvironmentObject var dataModel: DataModel
     
+    @State private var logoSize = 0.8
+    @State private var logoOpacity = 0.5
+    
     let viewName = String(describing: SplashView.self)
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [Color.purple, Color.orange], startPoint: .top, endPoint: .bottom)
+            LinearGradient(colors: [Color.black, Color.orange], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
-            VStack{
-                Text("Scene: \(viewName)")
-                    .padding()
-                
-                Spacer()
-        
-                Button() {
-                    // Action
-                    dataModel.incrementSceneChanges(amount: 1)
+            VStack {
+                VStack{
+                    // Logo Image
+                    Image(systemName: "hare.fill")
+                        .font(.system(size: 80))
+                        .foregroundColor(.yellow)
                     
-                    withAnimation {
-                        dataModel.stateView = ViewType.MenuView
-                    }
-                } label: {
-                    Text("Go to next View")
-                        .font(.largeTitle)
+                    // Logo Text
+                    Text("Cookie Clicker")
+                        .font(Font.custom("baskerville-bold", size: 26))
                         .foregroundColor(.white)
-                        .padding()
-                        .background(Color.gray)
-                        .cornerRadius(20.0)
+                        .opacity(0.8)
+                }
+                .scaleEffect(logoSize)
+                .opacity(logoOpacity)
+                .onAppear{
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.logoSize = 1.2
+                        self.logoOpacity = 1.0
+                    }
                 }
             }
-        }.transition(.slide)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation{
+                        dataModel.stateView = ViewType.MenuView
+                    }
+                }
+            }
+        }.transition(.opacity)
     }
 }
 
